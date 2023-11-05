d68b5b5b7f1c46ebc07b01921e8a7d13b95ef200
FraRussotto <russottofrancesco1@gmail.com>
2023-11-05 11:19:12 +0100

    [edit] random password generator

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
 
-if(isset($_POST['password_length']) && !empty($_POST['password_length'])){
-  echo 'Password check';
-}
+$msg = 'Scegliere password con almeno 8 caratteri e massimo 32 caratteri';
 
+$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567891?8%$^+-*/0)[]{}@#_=';
+
+$psw = '';
+
+$custom_alert = 'primary';
+
+if(isset($_POST['password_length']) && !empty($_POST['password_length'])){
+  if($_POST['password_length'] > 8 && $_POST['password_length'] < 32){
+    for($c = 0; $c < $_POST['password_length']; $c++){
+      //con un indice random vado a pescare i caratteri all'interno della variabile 'str_shuffle($chars)' in modo da ottenere una sequenza randomica
+      $index = rand(0, strlen(str_shuffle($chars)) - 1);
+      $char = $chars[$index];
+      $psw .= $char;
+      $msg = "La tua password è: <strong>$psw</strong>";
+      $custom_alert = 'success';
+    }
+  }
+  else{
+    $msg = 'La lunghezza deve essere compresa tra 8 e 32';
+    $custom_alert = 'danger';
+  }
+}
 
 ?>
 
@@ -24,16 +44,16 @@ if(isset($_POST['password_length']) && !empty($_POST['password_length'])){
 </head>
 <body>
 
-  <div class="container my-3">
+  <div class="container_fr my-3">
     <h1 class="text-center">Password generator</h1>
 
-    <div class="alert alert-success mt-5" role="alert">
-      Scegliere password con almeno 8 caratteri e massimo 32 caratteri
+    <div class="alert alert-<?php echo $custom_alert ?> mt-5" role="alert">
+      <?php echo $msg ?>
     </div>
 
     <div class="bg-white text-dark rounded-1 p-3 text-center ">
       <form action="index.php" method="POST">
-        <div class="input pb-3 d-flex justify-content-between align-items-center">
+        <div class="input pb-3 d-flex flex-column justify-content-center align-items-center">
           <p>Lunghezza password:</p>
           <input type="number" name="password_length" class="form-control input-w">
         </div>
